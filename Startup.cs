7719dc5616f0765f7d4fe8c5e0040db15b90426c
Flaspohler <cf639416@ohio.edu>
2020-10-09 11:12:33 -0400

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MIS4200ProjectTeam7.Startup))]
+namespace MIS4200ProjectTeam7
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
