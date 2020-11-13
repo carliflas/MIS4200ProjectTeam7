af783586e7f2c56d3312c556f0c31e202eb07b1c
Anthony Ciulli <ac111316@ohio.edu>
2020-11-13 16:19:44 -0500

    friday no errors email in up until page 3. rec still not counting

--- Controllers/CoreValuesController.cs
+++ Controllers/CoreValuesController.cs
@@ -9,6 +9,8 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using MIS4200ProjectTeam7.DAL;
 using MIS4200ProjectTeam7.Models;
+using System.Net;
+using System.Net.Mail;
 
 namespace MIS4200ProjectTeam7.Controllers
 {
@@ -150,6 +152,36 @@ namespace MIS4200ProjectTeam7.Controllers
             return View(coreValues);
         }
 
+        public ActionResult Email(int? id)
+        {
+            SmtpClient myClient = new SmtpClient();
+            // the following line has to contain the email address and password of someone
+            // authorized to use the email server (you will need a valid Ohio account/password
+            // for this to work)
+            myClient.Credentials = new NetworkCredential("AuthorizedUser", "UserPassword");
+            MailMessage myMessage = new MailMessage();
+            // the syntax here is email address, username (that will appear in the email)
+            MailAddress from = new MailAddress("luce@ohio.edu", "SysAdmin");
+            myMessage.From = from;
+            myMessage.To.Add("thomluce@gmail.com"); // this should be replaced with model data
+                                                    // as shown at the end of this document
+            myMessage.Subject = "MVC Email test";
+            // the body of the email is hard coded here but could be dynamically created using data
+            // from the model- see the note at the end of this document
+            myMessage.Body = "This is the body of the mail message. This can be essentially";
+myMessage.Body += "from the database, a variable, the return of another method...";
+            try
+            {
+                myClient.Send(myMessage);
+                TempData["mailError"] = "";
+            }
+            catch (Exception ex)
+            {
+                // this captures an Exception and allows you to display the message in the View
+                TempData["mailError"] = ex.Message;
+            }
+            return View();
+        }
         // POST: CoreValues/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
